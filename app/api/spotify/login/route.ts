fb2e6ce8248afe12388139532a5559037e4a36b7
Seth Keenan <seth.j.keenan@gmail.com>
2025-10-21 23:17:20 -0500

    test

--- app/api/spotify/login/route.ts
+++ app/api/spotify/login/route.ts
@@ -2,22 +2,31 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const clientId = process.env.SPOTIFY_CLIENT_ID!;
-  const scopes = ["user-read-email", "user-read-private", "playlist-read-private", "user-top-read", "playlist-modify-public", "playlist-modify-private"].join(" ");
+  const scopes = [
+    "user-read-email",
+    "user-read-private",
+    "playlist-read-private",
+    "user-top-read",
+    "playlist-modify-public",
+    "playlist-modify-private",
+  ].join(" ");
 
   const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://spot-data-ray3.vercel.app/api/spotify/callback"
-    : "http://127.0.0.1:3000/api/spotify/callback";
+    process.env.NODE_ENV === "production"
+      ? "https://spot-data-ray3.vercel.app"
+      : "http://127.0.0.1:3000";
 
-  const url =
-    "https://accounts.spotify.com/authorize?" +
-    new URLSearchParams({
-      client_id: clientId,
-      response_type: "code",
-      redirect_uri: baseUrl,
-      scope: scopes,
-      show_dialog: "true",
-    });
+  const redirectUri = `${baseUrl}/api/spotify/callback`;
 
-  return NextResponse.redirect(url);
+  const params = new URLSearchParams({
+    client_id: clientId,
+    response_type: "code",
+    redirect_uri: redirectUri,
+    scope: scopes,
+    show_dialog: "true",
+  });
+
+  const authUrl = `https://accounts.spotify.com/authorize?${params.toString()}`;
+
+  return NextResponse.redirect(authUrl);
 }
